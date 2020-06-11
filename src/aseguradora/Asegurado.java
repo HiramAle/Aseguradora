3d2b9a1a46feb64bfc3b04291437b03f630ba07a
HiramAle <>
2020-06-10 19:11:11 -0500

    Se agregó Asegurado y Transporte

---
+++ src/aseguradora/Asegurado.java
@@ -0,0 +1,70 @@
+package aseguradora;
+import java.util.Calendar;
+public class Asegurado {
+    
+    private String nombre,apellidoPaterno,apellidoMaterno,domicilio;
+    private String telefono[];
+    private Calendar fechaNacimiento;
+    private char RFC[] = new char [13];
+
+    public Asegurado() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String[] getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String[] telefono) {
+        this.telefono = telefono;
+    }
+
+    public Calendar getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Calendar fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public char[] getRFC() {
+        return RFC;
+    }
+
+    public void setRFC(char[] RFC) {
+        this.RFC = RFC;
+    }
+    
+    
+}
