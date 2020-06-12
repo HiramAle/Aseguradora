b4930e3356e53c8a82cf058635bdf84704d59c89
ale <aorozcoa1400@alumno.ipn.mx>
2020-06-11 19:00:28 -0500

    Modificacion Poliza

--- src/aseguradora/PolizaSeguro.java
+++ src/aseguradora/PolizaSeguro.java
@@ -19,11 +19,13 @@ public class PolizaSeguro extends Cobertura implements PrimaSeguro  {
     private Calendar  finVigencia;
     private String tipoPago;
 
+    private Transporte transport;
+    
     public PolizaSeguro() {
+        emision = Calendar.getInstance();
     }
-    
-    
 
+     
     // setters y getters 
     public int getNumero() {
         return numero;
@@ -64,12 +66,34 @@ public class PolizaSeguro extends Cobertura implements PrimaSeguro  {
     public void setTipoPago(String tipoPago) {
         this.tipoPago = tipoPago;
     }
+
+    public Transporte getTransport() {
+        return transport;
+    }
+
+    public void setTransport(Transporte transport) {
+        this.transport = transport;
+    }
+    
     
     
     //  implementacion de los metodos de la interfaz Prima seguro
     @Override
     public double calcularMontoPrima() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        double prima = 0.0;
+        prima += this.calularCostoBase(transport);
+        prima += super.calculaCostoCoberturas();
+        
+        //Comision por tipo de pago 
+        if (this.getTipoPago().equals("tarjeta")){
+          prima += prima * PrimaSeguro.comision;    
+        }
+        //Aumento por tipo de uso 
+        if (this.getTransport().tipo_uso.equals("comercial")){
+           prima += 2000.0;
+        }
+        
+        return prima;
     }
 
     @Override
@@ -79,7 +103,44 @@ public class PolizaSeguro extends Cobertura implements PrimaSeguro  {
 
     @Override
     public boolean aplicarDescuento(double porcentaje) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        double descuento = this.calcularMontoPrima() - (this.calcularMontoPrima() * (porcentaje/100));
+        return true;
+    }
+    
+    public double calularCostoBase(Transporte trans){
+        double costoBase = 0.0;
+        int mod = Integer.valueOf(trans.modelo);
+        /*
+        //Toma el costo base dependiendo del tipo de transporte 
+        if (trans instanceof Automovil){
+            if (mod < 2010 ) {
+                costoBase = PrimaSeguro.montoBaseAuto[0];
+            } else if (mod >= 2010 && mod <= 2016) {
+                costoBase = PrimaSeguro.montoBaseAuto[1];
+            } else if (mod >= 2017){
+                costoBase = PrimaSeguro.montoBaseAuto[2];
+            }
+        } else if (trans instanceof Motocicleta){
+            if (mod < 2010 ) {
+                costoBase = PrimaSeguro.montoBaseMoto[0];
+            } else if (mod >= 2010 && mod <= 2016) {
+                costoBase = PrimaSeguro.montoBaseMoto[1];
+            } else if (mod >= 2017){
+                costoBase = PrimaSeguro.montoBaseMoto[2];
+            }
+        } else if (trans instanceof Camion){
+            if (mod < 2010 ) {
+                costoBase = PrimaSeguro.montoBaseCamion[0];
+            } else if (mod >= 2010 && mod <= 2016) {
+                costoBase = PrimaSeguro.montoBaseCamion[1];
+            } else if (mod >= 2017){
+                costoBase = PrimaSeguro.montoBaseCamion[2];
+            }
+        }*/
+        
+        // verifica si hay aumento por la edad del asegurado
+        
+        return costoBase;
     }
     
     
