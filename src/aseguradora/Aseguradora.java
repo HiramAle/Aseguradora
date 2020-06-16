af4f0985143b9fa1b5c76da425392d5eae3d8198
ale <aorozcoa1400@alumno.ipn.mx>
2020-06-16 12:36:37 -0500

    Correccion formato fechas2

--- src/aseguradora/Aseguradora.java
+++ src/aseguradora/Aseguradora.java
@@ -12,9 +12,9 @@ public class Aseguradora {
 
         Transporte t1 = new Automovil("Seat", "2010", "M124", "FJGK1284", "MGKLY89", "comercial", "rojo", 15000.00, aseg1);
         Transporte t2 = new Motocicleta("Seat", "2018", "M124", "FJGK1284", "MGKLY89", "privado", "rojo", 15000.00, aseg2);
-        Transporte t3 = new Camion("KIA", "2017", "M124", "KSJF4628", "APFH52", "comercial", "verde", 15000.00, aseg3);
-        Transporte t4 = new Automovil("Toyota", "2019", "M248", "LDPJ2648", "PDEN46", "privado", "blanco", 15000.00, aseg4);
-        Transporte t5 = new Motocicleta("Yamaha", "2020", "M155", "OJFG1379", "PLOI12", "privado", "negro", 15000.00, aseg5);
+        Transporte t3 = new Camion("KIA", "2017", "M124", "KSJF4628", "APFH5267", "comercial", "verde", 15000.00, aseg3);
+        Transporte t4 = new Automovil("Toyota", "2019", "M248", "LDPJ2648", "PDEN466", "privado", "blanco", 15000.00, aseg4);
+        Transporte t5 = new Motocicleta("Yamaha", "2020", "M155", "OJFG1379", "PLOI162", "privado", "negro", 15000.00, aseg5);
 
         PolizaSeguro poliza1 = new PolizaSeguro(true, true, false, true, false, false, 12785, 15, 05, 2022, 15, 05, 2025, "efectivo", t1);
         PolizaSeguro poliza2 = new PolizaSeguro(false, true, false, false, true, false, 12785, 15, 05, 2022, 15, 05, 2025, "tarjeta", t2);
@@ -22,7 +22,7 @@ public class Aseguradora {
         PolizaSeguro poliza4 = new PolizaSeguro(false, false, false, true, true, false, 62348, 12, 10, 2022, 12, 10, 2025, "efectivo", t4);
         PolizaSeguro poliza5 = new PolizaSeguro(false, false, false, false, true, true, 48652, 4, 06, 2022, 4, 06, 2025, "tarjeta", t5);
 
-        double prima = poliza2.calcularMontoPrima();
+        double prima = poliza4.calcularMontoPrima();
         System.out.println(prima);
     }
 
